68ddba463bf795ab31e35a8c8e988a48dfb6eb1b
Shigal <shiglaws@gmail.com>
2022-12-08 10:24:14 +0530

    first commit

---
+++ src/main/java/GUI.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    int count = 0;
+    private JLabel label;
+    private JPanel panel;
+    private JFrame frame;
+    private JButton button;
+
+    public GUI(){
+        frame = new JFrame();
+        panel = new JPanel();
+        button = new JButton("Click Me!");
+        button.addActionListener(this);
+        label = new JLabel("Number of clicks: 0");
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0,1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("My GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
+}
